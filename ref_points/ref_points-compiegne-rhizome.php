efce4ca262e1a458ab22d4cd48ad679441fe2303
Jocelyn Delande <jocelyn at delalande dot fr>
2013-11-24 16:35:56 +0100

    added altitudes of buildings (estimated to ref_points)

--- ref_points/ref_points-compiegne-rhizome.php
+++ ref_points/ref_points-compiegne-rhizome.php
@@ -1,13 +1,13 @@
 <?php
 $ref_points = array (
-  "Église saint-germain" => array(49.413518,2.815479, 37),
-  "Mairie de Compiègne" => array(49.417842,2.826045, 42),
-  "Pylône TDF mont-ganelon" => array(49.444393,2.852277, 175),
-  "UTC - Benjamin Franklin (batiment A, antenne Graf'Hit)" => array(49.415573,2.818663, 36),
-  "Église Saint-Paul des sablons (quartier de la victoire)" => array(49.404308,2.821616, 57),
-  "Pavillon de l'hâbitat" => array(49.394198,2.78987, 34),
-  "Tour rose First-location (ZAC Mercières)" => array(49.389909,2.789854, 32),
-  "Église saint-antoine" => array(49.415837,2.823295, 40),
-  "Église saint-jacques" => array(49.417032,2.82789, 46),
+  "Église saint-germain" => array(49.413518,2.815479, 37+15),
+  "Mairie de Compiègne" => array(49.417842,2.826045, 42+24),
+  "Pylône TDF mont-ganelon" => array(49.444393,2.852277, 135+49),
+  "UTC - Benjamin Franklin (batiment A, antenne Graf'Hit)" => array(49.415573,2.818663, 36+21),
+  "Église Saint-Paul des sablons (quartier de la victoire)" => array(49.404308,2.821616, 57+24),
+  "Pavillon de l'hâbitat" => array(49.394198,2.78987, 34+12),
+  "Tour rose First-location (ZAC Mercières)" => array(49.389909,2.789854, 32+30),
+  "Église saint-antoine" => array(49.415837,2.823295, 40+20),
+  "Église saint-jacques" => array(49.417032,2.82789, 46+30),
 );
 ?>
