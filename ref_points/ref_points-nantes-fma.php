ff113d4d669a88bf9db445bc769168b321e97406
Jocelyn Delande <jocelyn at delalande dot fr>
2014-09-06 19:58:43 +0200

    added grue titan refpoint + untabified that file

--- ref_points/ref_points-nantes-fma.php
+++ ref_points/ref_points-nantes-fma.php
@@ -5,9 +5,13 @@ $ref_points = array (
                      'Ecole archi' => array(47.207, -1.557361, 30+6.000),
                      'Palais de justice' => array(47.207639, -1.5615, 25+6.000),
                      'Blockhaus DY10' => array(47.206528, -1.563861, 7+6.000),
-                     'Cite radieuse' => array(47.18875, -1.568111, 52+17.000),
-					 'HOST' => array(47.17826,-1.56448, 35.938+20), // 5 grands étages
-					 'HITS' => array(47.230965,-1.63146, 12+51), // 3 grands étages
-					 'Cogent' => array(47.20319,-1.55025, 16+10) // 4 grands étages
-					 );
-?>
+                     'Cite radieuse' => array(47.18875, -1.568111, 23+52),
+                     'HOST' => array(47.19668,-1.56072, 4.24+8), // 2 étages + toit
+                     'HITS' => array(47.230965,-1.63146, 12+51), // 3 grands étages
+                     'Cogent' => array(47.20319,-1.55025, 16+10), // 4 grands étages
+                     'Église Saint-Clément' => array(47.221384,-1.546886, 80+23), // http://fr.wikipedia.org/wiki/%C3%89glise_Saint-Cl%C3%A9ment_de_Nantes
+                     'Grue titan jaune' => array(47.20508,-1.56972, 6.3+43), // http://fr.wikipedia.org/wiki/Grue_Titan
+                     'Ouzmoutous (immeuble, ~Pirmil)' => array(47.1938647,-1.5401393, 12.0+16*3), // Immeuble de 16 étages
+                     'Siege social FAIMaison' => array(47.2261966,-1.5432599,20+40) // 5 étages immeuble;
+
+);?>
